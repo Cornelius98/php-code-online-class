c646311274fb3a5b73ec7c7bf99ae9bef43257e7
Cornelius Kasokola <41131476+Cornelius98@users.noreply.github.com>
2025-08-07 10:03:21 -0800

    Source code for arithmetic operators

---
+++ arithmetic_operators.php
@@ -0,0 +1,47 @@
+<?php
+//Addition (+)
+$a = 10;
+$b = 20;
+$c = 10;
+$sum = $a + $b + $c;
+echo "<br>The sum is: ".$sum."<br>";
+
+//Subtraction (-)
+$j = 100;
+$k = 200;
+$difference = $k-$j;
+echo "The difference is: ".$difference."<br>";
+
+//Multiplication (*)
+$m = 10;
+$n = 10;
+const PI = 3.142;
+$product = $m * ($n * PI);
+echo "The product is: ".$product."<br>";
+
+//Division (/)
+$dividor_1 = 10;
+$dividor_2 = 5;
+$quotient = $dividor_1/$dividor_2;
+echo "The quotient is: ".$quotient."<br>";
+
+//Modudulus (%)
+$remainder = $dividor_1%$dividor_2;
+echo "The modulus (remainder): ".$remainder."<br>";
+
+
+//Concatenation (joining)
+$h = "Hellow";
+$w = "World";
+$sentence = $h." ".$w;
+echo $sentence;
+
+
+//Power (Exponential)
+$base = 2;
+$expo = 2;
+echo "<br> First exponential notation: ".$base**$expo."<br>";
+
+
+echo pow($base,$expo)
+?>
