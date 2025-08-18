d81d6e0955e5776eded20220e32ccd5f93379a83
Cornelius Kasokola <41131476+Cornelius98@users.noreply.github.com>
2025-08-18 10:20:38 -0800

    SQL Code

---
+++ schema.sql
@@ -0,0 +1,9 @@
+--- Creating a database
+CREATE DATABASE `scratch_db`;
+
+--- Creating a table
+CREATE TABLE `person`(
+   `name` VARCHAR(30) NOT NULL,
+   `height` VARCHAR(3) NOT NULL,
+   `city` VARCHAR(30) NOT NULL
+);
